39422224823f06a9619de9c6d379b44cfda2c168
Daniyar Turak <dany.turak@gmail.com>
2021-10-30 10:43:50 +0600

    uppdate, delete courses

--- routes/courses.js
+++ routes/courses.js
@@ -1,11 +1,16 @@
 const express = require('express');
 
 const { 
-    getCourses
+    getCourses,
+    getCourse,
+    addCourse,
+    updateCourse,
+    deleteCourse
 } = require('../controllers/courses');
 
 const router = express.Router({ mergeParams: true });
 
-router.route('/').get(getCourses);
+router.route('/').get(getCourses).post(addCourse);// bootcamp router has :bootcampid/courses ,it will transfer here because it is post
+router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse); 
 
 module.exports = router;
