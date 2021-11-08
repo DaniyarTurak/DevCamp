96ad7994283bdef8bcdb3b29c33a575da1c8dba6
Daniyar Turak <dany.turak@gmail.com>
2021-11-08 11:48:36 +0600

    ownerships of bootcamp,course

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -31,6 +31,19 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 // @route   POST /api/v1/bootcamps
 // @access  Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+    // Add user to req.body
+    req.body.user = req.user.id;
+
+    // Check for published bootcamp
+    // Find all bootcamps that created that user
+    const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+    // findOne({id: id}) is equivelant to findById(id), but on;y differnece is they handle undefined diffirently, findOne would handle it as undefined while findById as null
+
+    // If the user is not admin, they can only add one bootcamp
+    if (publishedBootcamp && req.user.role !== 'admin') {
+        return next(new ErrorResponse(`User with id: ${req.user.id} has already published a bootcamp`, 400));
+    }
+
     const bootcamp = await Bootcamp.create(req.body);
     res.status(201).json({
         success: true,
@@ -43,14 +56,22 @@ exports.createBootcamp = asyncHandler(async (req, res, next) => {
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
-    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true
-    });
+    let bootcamp = await Bootcamp.findById(req.params.id);
     if (!bootcamp) {
         return next(new ErrorResponse(`Bootcamp is not found with id of ${req.params.id}`, 404));
     }
-    res.status(200).json({ success: true, count: bootcamp.length, data: bootcamp });
+
+    // Make sure user is bootcamp owner
+    if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(new ErrorResponse(`User ${req.params.id} is not authorized to update this bootcamp`, 401));
+    }
+
+    bootcamp = await Bootcamp.findOneAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
+
+    res.status(200).json({ success: true, data: bootcamp });
 });
 
 // @desc    Delete bootcamp
@@ -60,6 +81,11 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
     const bootcamp = await Bootcamp.findById(req.params.id); // findByIdAndDelete is not trigger by pre()
     if (!bootcamp) {
         return next(new ErrorResponse(`Bootcamp is not found with id of ${req.params.id}`, 404));
+    }
+
+    // Make sure user is bootcamp owner
+    if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(new ErrorResponse(`User ${req.params.id} is not authorized to delete this bootcamp`, 401));
     }
 
     bootcamp.remove(); // in model u can see pre('remove');
@@ -103,6 +129,11 @@ exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
 
     if (!bootcamp) {
         return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`), 404);
+    }
+
+    // Make sure user is bootcamp owner
+    if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(new ErrorResponse(`User ${req.params.id} is not authorized to upload photo to this bootcamp`, 401));
     }
 
     if (!req.files) {
--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config({ path: './config/config.env' });
 //Load models
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
+const User = require('./models/User');
 
 //Connect to Db
 mongoose.connect(process.env.MONGO_URI, {
@@ -22,12 +23,16 @@ const bootcamps = JSON.parse(
 const courses = JSON.parse(
     fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
 );
+const users = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/users.json`)
+);
 
 //Import into Db
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
         await Course.create(courses);
+        await User.create(users);
         console.log('Data imported...'.green.inverse);
         process.exit();
 
@@ -41,6 +46,7 @@ const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
         await Course.deleteMany();
+        await User.deleteMany();
 
         console.log('Data destroyed...'.red.inverse);
         process.exit();
